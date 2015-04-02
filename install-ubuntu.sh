9ab1a090e1a07736cd5ded82b03e47317cab0c18
Tilman Schlenker <tilman@mailbox.org>
2015-04-02 13:51:41 +0200

    ubuntu script

---
+++ install-ubuntu.sh
@@ -0,0 +1,21 @@
+# Ask for the user password
+# Script only works if sudo caches the password for a few minutes
+sudo true
+ 
+# Add Docker PPA and install latest version
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get install lxc-docker -y
+ 
+# Install docker-compose
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/1.1.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+ 
+# Install docker-cleanup command
+cd /tmp
+git clone https://gist.github.com/76b450a0c986e576e98b.git
+cd 76b450a0c986e576e98b
+sudo mv docker-cleanup /usr/local/bin/docker-cleanup
+sudo chmod +x /usr/local/bin/docker-cleanup
